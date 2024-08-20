382a6fcbe69fad1a437364ebe00cf087d5b2303f
jonghyuk <kasiruske@naver.com>
2024-08-20 23:36:56 +0900

    first commit

---
+++ ASP.NETMVC_test/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NETMVC_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
