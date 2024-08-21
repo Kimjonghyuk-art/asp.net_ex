a66842918c16e7d339dbc37929e735e88cd22c97
kimjonghyuk <kasiruske@naver.com>
2024-08-21 23:16:45 +0900

    DB connection test

---
+++ ASP.NETMVC_test/Dao/UserDao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ASP.NETMVC_test.Models;
+using MySql.Data.MySqlClient;
+
+namespace ASP.NETMVC_test.Dao
+{
+	public class UserDao
+	{
+		public UserDao()
+		{
+		}
+
+		public List<User> getUsers()
+		{
+			List<User> list = new List<User>();
+			string SQL = "SELECT * FROM USER ";
+			using (MySqlConnection conn = GetConnection())
+			{
+				conn.Open();
+				MySqlCommand cmd = new MySqlCommand(SQL, conn);
+
+				using(var reader = cmd.ExecuteReader())
+				{
+					while(reader.Read())
+					{
+						list.Add(new Data()
+						{
+							UserId = Convert.ToInt32(reader["UserId"])
+						});
+					}
+				}
+			}
+			conn.close();
+
+			return list;
+		}
+
+    private MySqlConnection GetConnection()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
+
--- ASP.NETMVC_test/Models/User.cs
+++ ASP.NETMVC_test/Models/User.cs
@@ -6,6 +6,10 @@ namespace ASP.NETMVC_test.Models
 			public int UserId { get; set; }
 
 			public string UserName { get; set; }
-	}
+					
+			public int Age { get; set; }
+
+			public string Email { get; set; }
+  }
 }
 
